b353ee5a82bf1b0c9237ddc87c4536f71221f5f0
Fazakas Alexandra <alexandra.fazakas91@gmail.com>
2020-07-18 10:16:23 +0300

    scroll by keys removed

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import { Game } from "./classes/Game.js"
 
+document.onkeydown = KD;
+       function KD(e) {
+         event.returnValue = false;
+}
 
 let gameMap = document.getElementById('container')
 let gameInfo = document.getElementById('info-container')
